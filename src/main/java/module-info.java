7c6e35a9b9910a199e7df28796889c36deda84ca
Margaret Toebes <margaret.toebes@gmail.com>
2021-04-30 14:27:46 -0400

    Create module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module testJavaOpenCV.main {
+    requires javafx.controls;
+    requires javafx.swing;
+    requires opencv;
+    exports team2901.basic;
+    exports team2901.javafx;
+    exports team2901.opencv;
+}
